7f8354777e939513da5c5d1d5ba5106bca38c8db
Brandon Webb <webb.c.brandon@gmail.com>
2018-02-21 08:48:35 -0500

    grrr

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import Exception, { codes } from './src/exception';
+const error = {
+  Exception,
+  codes,
+}
 
 export { Server } from './src/main';
 export { Logger } from './src/utils/logger';
-export {
-  error: {
-    Exception,
-    codes,
-  }
-}
+export { error }
