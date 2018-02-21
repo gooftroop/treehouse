b54502bead4a727d49a1aff206431c21c7119f03
Brandon Webb <webb.c.brandon@gmail.com>
2018-02-21 07:46:59 -0500

    not exporting as lib but submodule now, so use es6

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-require = require("@std/esm")(module)
-
-const Server = require('./src/main');
-const Logger = require('./src/utils/logger');
-const error = require('./src/exception');
-
-module.exports = {
-  "error": error,
-  "Logger": Logger,
-  "Server": Server,
-}
+export Server from './src/main';
+export Logger from './src/utils/logger';
+export * as error from './src/exception';
