b288ec25e76fee27fbe7e42016ba7657054f12ca
Brandon Webb <webb.c.brandon@gmail.com>
2018-03-06 21:26:01 -0500

    adding default configs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-/* eslint-disable import/first */
+import defaults from 'configuration/default';
 import Exception from 'axon/exception';
 import Logger from 'axon/utils/logger';
 import Server from 'axon/server';
 
 import * as codes from 'axon/exception/codes';
-/* eslint-enable import/first */
 
 export default Server;
-export { Exception, Logger, codes };
+export { defaults, Exception, Logger, codes };
