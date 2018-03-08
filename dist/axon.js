e52b0b444fdc28b013bb1422da45ea2dff2515b2
Brandon Webb <webb.c.brandon@gmail.com>
2018-03-07 21:21:04 -0500

    fixing error middleware

--- dist/axon.js
+++ dist/axon.js
@@ -505,7 +505,6 @@ exports.default = (0, _koaMorgan2.default)(formatter, {
 
 
 exports.__esModule = true;
-exports.default = errorMiddleware;
 
 var _logger = __webpack_require__("./src/utils/logger.js");
 
@@ -513,17 +512,52 @@ var _logger2 = _interopRequireDefault(_logger);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 var LOGGER = _logger2.default.getLogger('root');
 
 /**
- * [errorParser description]
- * @param  {[type]}   e     [description]
- * @param  {[type]}   ctx   [description]
- * @return {[type]}         [description]
+ * [errorMiddleware description]
+ * @param  {[type]}   ctx  [description]
+ * @param  {Function} next [description]
+ * @return {[type]}        [description]
  */
-function errorMiddleware(e, ctx) {
-  return ctx ? LOGGER.error(e, ctx) : LOGGER.error(e);
-}
+
+exports.default = function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(ctx, next) {
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.prev = 0;
+            _context.next = 3;
+            return next();
+
+          case 3:
+            _context.next = 11;
+            break;
+
+          case 5:
+            _context.prev = 5;
+            _context.t0 = _context['catch'](0);
+
+            LOGGER.error(_context.t0, ctx);
+            ctx.status = _context.t0.status || 500;
+            ctx.body = _context.t0.message;
+            ctx.app.emit('error', _context.t0, ctx);
+
+          case 11:
+          case 'end':
+            return _context.stop();
+        }
+      }
+    }, _callee, undefined, [[0, 5]]);
+  }));
+
+  return function (_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+}();
 
 /***/ }),
 
--- src/middleware/error.js
+++ src/middleware/error.js
@@ -3,11 +3,18 @@ import Logger from 'axon/utils/logger';
 const LOGGER: Object = Logger.getLogger('root');
 
 /**
- * [errorParser description]
- * @param  {[type]}   e     [description]
- * @param  {[type]}   ctx   [description]
- * @return {[type]}         [description]
+ * [errorMiddleware description]
+ * @param  {[type]}   ctx  [description]
+ * @param  {Function} next [description]
+ * @return {[type]}        [description]
  */
-export default function errorMiddleware(e: Error, ctx: Object): void {
-  return ctx ? LOGGER.error(e, ctx) : LOGGER.error(e);
-}
+export default async (ctx: Object, next: Function): void => {
+  try {
+    await next();
+  } catch (e) {
+    LOGGER.error(e, ctx);
+    ctx.status = e.status || 500;
+    ctx.body = e.message;
+    ctx.app.emit('error', e, ctx);
+  }
+};
